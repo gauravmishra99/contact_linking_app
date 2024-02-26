efde4591aacea646eb94589c558fdea77a32e046
gauravmishra99 <gauravmishra.190699@gmail.com>
2024-02-26 12:25:33 +0530

    online postgres connection added

--- src/db/db_init.ts
+++ src/db/db_init.ts
@@ -1,11 +1,5 @@
 import { Pool } from 'pg'
 
 export default new Pool({
-  host: 'localhost',
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database : process.env.DB_DATABASE,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionString : process.env.DB_CONNECTION_URI
 })
